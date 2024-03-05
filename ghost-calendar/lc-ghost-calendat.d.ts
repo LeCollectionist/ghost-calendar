9cd6fa09da4ea6d92ffec01795fb9786eb9ee004
Julien Queffelec <julien.queffelec@lecollectionist.com>
2024-03-05 10:01:35 +0100

    fix(declaration): revert

--- ghost-calendar/lc-ghost-calendat.d.ts
+++ ghost-calendar/lc-ghost-calendat.d.ts
@@ -1,13 +1,1 @@
-declare module "ghost-calendar/lc-ghost-calendar" {
-  export default class LcGhostCalendar extends HTMLElement {
-    constructor();
-    connectedCallback(): void;
-    disconnectedCallback(): void;
-    attributeChangedCallback(
-      name: string,
-      oldValue: string,
-      newValue: string
-    ): void;
-    static get observedAttributes(): string[];
-  }
-}
+declare module "lc-ghost-calendar" {}
