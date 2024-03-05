a90f893e07c57ed2f39ad5cd6a7b479280fa139e
Julien Queffelec <julien.queffelec@lecollectionist.com>
2024-03-05 10:00:47 +0100

    chore(version): bump version to 1.8.1

--- ghost-calendar/lc-ghost-calendat.d.ts
+++ ghost-calendar/lc-ghost-calendat.d.ts
@@ -1,1 +1,13 @@
-declare module "lc-ghost-calendar" {}
+declare module "ghost-calendar/lc-ghost-calendar" {
+  export default class LcGhostCalendar extends HTMLElement {
+    constructor();
+    connectedCallback(): void;
+    disconnectedCallback(): void;
+    attributeChangedCallback(
+      name: string,
+      oldValue: string,
+      newValue: string
+    ): void;
+    static get observedAttributes(): string[];
+  }
+}
